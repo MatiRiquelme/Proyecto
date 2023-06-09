a4a27e03a0119d3a8f1fa4caa9258c8350a13f0f
Matias <riquelmemati>
2023-06-08 21:39:47 -0400

    actualizado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('estudiantes')->insert([
+            ['nombre'=>'']
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
     }
 }
